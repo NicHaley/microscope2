a1cc8da4a43e5e49d3c14e7ec8e38b71830d2bc2
Nicholas Haley <nicholaswilliamhaley@gmail.com>
2015-01-25 14:21:59 -0500

    Wait on the post subscription

--- microscope2/lib/router.js
+++ microscope2/lib/router.js
@@ -1,5 +1,7 @@
-Router.confingure({
-	layoutTemplate: 'layout'
+Router.configure({
+	layoutTemplate: 'layout',
+	loadingTemplate: 'loading',
+	waitOn: function() { return Meteor.subscribe('posts'); }
 });
 
-Router.route('/', {name: 'postList'});
+Router.route('/', {name: 'postsList'});
