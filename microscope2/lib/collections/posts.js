fbb71e1ccdcd3f38bf33aba2eea7136b93ae86f9
Nicholas Haley <nicholaswilliamhaley@gmail.com>
2015-01-26 17:07:52 -0500

    Use a method to submit the post.

--- microscope2/lib/collections/posts.js
+++ microscope2/lib/collections/posts.js
@@ -1,8 +1,28 @@
 Posts = new Mongo.Collection('posts');
 
-Posts.allow({
-	insert: function(userId, doc) {
-    	// only allow posting if you are logged in
-		return !! userId;
+Meteor.method({
+	postInsert: function(postAttributes) {
+		check(Meteor.userId(), String);
+		check(postAttributes, {
+			title: String,
+			url: String
+		});
+
+		var user = Meteor.user();
+
+		// The _extend attribute allows us to extend the post object
+		// defined in post_submit with the properties of the object
+		// defined below
+		var post = _.extend(postAttributes, {
+			userId: user._id;
+			author: user.username,
+			submitted: new Date()
+		});
+
+		var postId = Posts.insert(post);
+
+		return {
+			_id: postId
+		};
 	}
 });
