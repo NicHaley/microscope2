6098ef94be61e493f10410682d0d7c2c4be399d6
Nicholas Haley <nicholaswilliamhaley@gmail.com>
2015-01-27 11:22:30 -0500

    Created a form to submit comments.

--- microscope2/server/fixtures.js
+++ microscope2/server/fixtures.js
@@ -12,13 +12,12 @@ if (Posts.find().count() === 0) {
   	});
 	var sacha = Meteor.users.findOne(sachaId);
 
-
-	Posts.insert({
+	var telescopeId = Posts.insert({
 		title: 'Introducing Telescope',
 		userId: sacha._id,
 		author: sacha.profile.name,
-    	url: 'http://sachagreif.com/introducing-telescope/',
-    	submitted: new Date(now - 7 * 3600 * 1000)
+		url: 'http://sachagreif.com/introducing-telescope/', 
+		submitted: new Date(now - 7 * 3600 * 1000)
 	});
 
 	Posts.insert({
